a7b7c7b8f4abb800b2ada58a0a92b77648a876f0
Ghada Emad <ghadaemad011@gmail.com>
2025-05-10 03:03:45 +0300

    authenticate how see navbar content only login users

--- ecommerce-dashboard/src/app/services/auth/auth.service.ts
+++ ecommerce-dashboard/src/app/services/auth/auth.service.ts
@@ -1,13 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { afterNextRender, inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Auth, AuthForgetPassword, AuthLogin, AuthResetPassword } from '../../interfaces/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  // same as observable but with variables
+  userInfo:BehaviorSubject<string|null>=new BehaviorSubject<string|null>('');
   _httpClient = inject(HttpClient);
+  constructor(){
+    //handle localstorage to work in browser .
+    afterNextRender(()=>{
+      if(localStorage.getItem('token')){
+        this.userInfo.next(localStorage.getItem('token'));
+      }
+    })
+  }
   register(formData:FormData): Observable<any> {
     return this._httpClient.post(
       'http://localhost:5000/api/auth/users/signup',
@@ -30,4 +40,8 @@ export class AuthService {
       'http://localhost:5000/api/auth/users/reset-password',info
     );
   }
+  saveUser(){
+    this.userInfo.next(localStorage.getItem('token'));
+  }
+  
 }
